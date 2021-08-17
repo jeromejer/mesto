c18f2e18c764502eab89ef95e0a5035241e05726
Lazukova Aleksandra <jeromejer@ya.ru>
2021-08-17 17:47:04 +0000

    fix: correction after review

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,32 @@
 let modal = document.querySelector('.popup');
-
+let textName = document.querySelector('[name="text_name"]');
+let textJob = document.querySelector('[name="text_job"]');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let formElement = modal.querySelector('.form')
 let openBtn = document.querySelector('.profile__edit');
-openBtn.addEventListener('click', function () {
-  modal.classList.add('popup_open');
-})
+let closeBtn = document.querySelector('.popup__close');
 
+//функция открывает popup
+function openModal() { 
+  modal.classList.add('popup_open');
+  textName.value = profileName.textContent;
+  textJob.value = profileJob.textContent
+}
+
+//функция закрывает popup
 function modalClose() {
   modal.classList.remove('popup_open');
 }
 
-let closeBtn = document.querySelector('.popup__close');
-closeBtn.addEventListener('click', modalClose)
-
-let formElement = modal.querySelector('.form')
-let textName = document.querySelector('.form__text_name');
-let textJob = document.querySelector('.form__text_job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-
-function formSubmitHandler (evt) {
+//функция отправки формы
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = textName.value;
-  profileJob.textContent = textJob.value; 
+  profileJob.textContent = textJob.value;
   modalClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+openBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', modalClose);
