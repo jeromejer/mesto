f649cfc2e04fd3fbf0cca211dff1cc3b6cc592bf
Alexandra Lazukova <jeromejer@yandex.ru>
2021-10-10 16:53:30 +0700

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,44 +21,44 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name].[hash][ext]',
-        }
-      },
-      {
-        test: /\.(svg)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name].[hash][ext]',
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name].[hash][ext]',
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-            MiniCssExtractPlugin.loader, 
-            {
-                loader: 'css-loader',
-                options: {
-                   importLoaders: 1
-                }
-            },
-          'postcss-loader'
-        ]
-      },
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {
+      test: /\.(png|jpg|jpeg|gif)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name].[hash][ext]',
+      }
+    },
+    {
+      test: /\.(svg)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name].[hash][ext]',
+      }
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'fonts/[name].[hash][ext]',
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader'
+      ]
+    },
     ]
   },
   plugins: [
